6d3483ac39d3b579ea18f3043c54ae7bdea18554
cassLaffan <poisonberryvines@gmail.com>
2024-09-01 09:40:27 -0400

    Adding the first of the examples

---
+++ Week_1/Factorial.cpp
@@ -0,0 +1,22 @@
+#include "./Factorial.h"
+
+// Note: recursion is one of those times where we can break the
+// one entry, one exit principle.
+int factorial(int num){
+    // This is what we call a base case.
+    // A base case is the "default behaviour" of a function
+    // This has to happen at some point to break out of the "loop"
+    if(num <= 1){
+        return 1;
+    }
+    // This is the recursive step. The step that calls the function
+    // internally is where it becomes recursive.
+    else{
+        return num * (factorial(num - 1));
+    }
+}
+
+int main(){
+    printf("The factorial of 10 is: %d\n", factorial(10));
+    return 0;
+}
---
+++ Week_1/Factorial.h
@@ -0,0 +1,18 @@
+#ifndef RECURSION_EXAMPLE_H
+#define RECURSION_EXAMPLE_H
+
+#include <iostream>
+
+/*
+* This is a classic example of a problem that can be solved
+* with recursion. 
+* Factorials are defined as "products" (multiples) of a number
+* and all of its preceeding positive integers.
+*
+* Factorials follow the formula of:
+* n! = n * (n-1) * (n-2) * ... * 2 * 1
+*/
+
+int factorial(int);
+
+#endif
